742623ff90bd7a70233fd00ec85f2744c59020bd
Mehedi Hasan <mehedi08h@gmail.com>
2024-12-23 17:25:12 +0600

    setup client

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <Button title="Hello">Hello</Button>
+        </div>
+    );
+};
+
+export default Home;
