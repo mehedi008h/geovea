194782256bd8399583e9e657d6ef1ef15713f4ff
Mehedi Hasan <mehedi08h@gmail.com>
2025-01-08 17:37:59 +0600

    redegine product table

--- client/components/admin/product/Products.tsx
+++ client/components/admin/product/Products.tsx
@@ -1,171 +1,194 @@
 "use client";
 
-import * as React from "react";
+import React, { useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FaRegEye } from "react-icons/fa6";
+import { CiEdit } from "react-icons/ci";
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import CustomeTable from "@/components/common/CustomeTable";
+import { Badge } from "@/components/ui/badge";
+import Modal from "@/components/common/Model";
 
-const data: Payment[] = [
+const data: Product[] = [
     {
-        id: "m5gr84i9",
-        amount: 316,
-        status: "success",
-        email: "ken99@yahoo.com",
+        _id: "m5gr84i9",
+        name: "Product name provided",
+        category: "Category",
+        price: 19.99,
+        stock: 100,
+        image: "/images/product1.jpg",
     },
     {
-        id: "3u1reuv4",
-        amount: 242,
-        status: "success",
-        email: "Abe45@gmail.com",
-    },
-    {
-        id: "derv1ws0",
-        amount: 837,
-        status: "processing",
-        email: "Monserrat44@gmail.com",
-    },
-    {
-        id: "5kma53ae",
-        amount: 874,
-        status: "success",
-        email: "Silas22@gmail.com",
-    },
-    {
-        id: "bhqecj4p",
-        amount: 721,
-        status: "failed",
-        email: "carmella@hotmail.com",
+        _id: "m5gr84i9",
+        name: "Product",
+        category: "Category",
+        price: 19.99,
+        stock: 4,
+        image: "/images/product1.jpg",
     },
 ];
 
-export type Payment = {
-    id: string;
-    amount: number;
-    status: "pending" | "processing" | "success" | "failed";
-    email: string;
+export type Product = {
+    _id: string;
+    image: string;
+    name: string;
+    category: string;
+    price: number;
+    stock: number;
 };
 
-export const columns: ColumnDef<Payment>[] = [
-    {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) =>
-                    table.toggleAllPageRowsSelected(!!value)
-                }
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-    {
-        accessorKey: "status",
-        header: "Status",
-        cell: ({ row }) => (
-            <div className="capitalize">{row.getValue("status")}</div>
-        ),
-    },
-    {
-        accessorKey: "email",
-        header: ({ column }) => {
-            return (
-                <Button
-                    variant="ghost"
-                    onClick={() =>
-                        column.toggleSorting(column.getIsSorted() === "asc")
+enum TYPE {
+    VIEW = "view",
+    EDIT = "edit",
+}
+
+export const Products = () => {
+    const [open, setOpen] = useState<boolean>(false);
+    const [modalType, setModalType] = useState<TYPE>(TYPE.VIEW);
+
+    const columns: ColumnDef<Product>[] = [
+        {
+            id: "select",
+            header: ({ table }) => (
+                <Checkbox
+                    checked={
+                        table.getIsAllPageRowsSelected() ||
+                        (table.getIsSomePageRowsSelected() && "indeterminate")
                     }
-                >
-                    Email
-                    <ArrowUpDown />
-                </Button>
-            );
+                    onCheckedChange={(value) =>
+                        table.toggleAllPageRowsSelected(!!value)
+                    }
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
         },
-        cell: ({ row }) => (
-            <div className="lowercase">{row.getValue("email")}</div>
-        ),
-    },
-    {
-        accessorKey: "amount",
-        header: () => <div className="text-right">Amount</div>,
-        cell: ({ row }) => {
-            const amount = parseFloat(row.getValue("amount"));
-
-            // Format the amount as a dollar amount
-            const formatted = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-            }).format(amount);
-
-            return <div className="text-right font-medium">{formatted}</div>;
+        {
+            accessorKey: "image",
+            header: "Image",
+            cell: ({ row }) => (
+                <div className="h-12 w-12 rounded-md bg-neutral-200"></div>
+            ),
         },
-    },
-    {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row }) => {
-            const payment = row.original;
+        {
+            accessorKey: "name",
+            header: "Name",
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("name")}</div>
+            ),
+        },
+        {
+            accessorKey: "price",
+            header: () => <div className="text-right">Price</div>,
+            cell: ({ row }) => {
+                const amount = parseFloat(row.getValue("price"));
 
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() =>
-                                navigator.clipboard.writeText(payment.id)
-                            }
+                // Format the amount as a dollar amount
+                const formatted = new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                }).format(amount);
+
+                return (
+                    <div className="text-right font-medium">{formatted}</div>
+                );
+            },
+        },
+        {
+            accessorKey: "category",
+            header: "Category",
+            cell: ({ row }) => (
+                <Badge variant="destructive">{row.getValue("category")}</Badge>
+            ),
+        },
+        {
+            accessorKey: "stock",
+            header: "Stock",
+            cell: ({ row }) => {
+                const stock = parseInt(row.getValue("stock"));
+                return (
+                    <div className="lowercase">
+                        {stock > 5 ? (
+                            <span className="text-green-600 font-medium">
+                                {stock}
+                            </span>
+                        ) : (
+                            <span className="text-red-600 font-medium">
+                                {stock}
+                            </span>
+                        )}
+                    </div>
+                );
+            },
+        },
+        {
+            id: "actions",
+            header: "Actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                return (
+                    <div className="flex gap-2">
+                        <Button
+                            onClick={() => handleModal(TYPE.VIEW)}
+                            size="icon"
+                            className="bg-blue-500 hover:bg-blue-600 text-white"
                         >
-                            Copy payment ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>
-                            View payment details
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            );
+                            <FaRegEye />
+                        </Button>
+                        <Button
+                            onClick={() => handleModal(TYPE.EDIT)}
+                            size="icon"
+                            className="bg-orange-500 hover:bg-orange-600 text-white"
+                        >
+                            <CiEdit />
+                        </Button>
+                        <Button
+                            size="icon"
+                            className="bg-red-500 hover:bg-red-600 text-white"
+                        >
+                            <AiOutlineDelete />
+                        </Button>
+                    </div>
+                );
+            },
         },
-    },
-];
+    ];
 
-const Products = () => {
+    // handle modal
+    const handleModal = (type: TYPE) => {
+        setModalType(type);
+        setOpen(true);
+    };
+
+    // render the component
     return (
         <div>
-            <CustomeTable<Payment>
+            <CustomeTable<Product>
                 data={data}
                 columns={columns}
-                searchBy="email"
-                searchPalceholder="Search by email"
+                searchBy="name"
+                searchPalceholder="Search by name"
             />
+            <Modal
+                open={open}
+                setOpen={setOpen}
+                className="h-screen w-[30%] bg-neutral-800"
+                title={`Product Details - ${modalType}`}
+            >
+                {modalType === TYPE.VIEW && <div>View</div>}
+                {modalType === TYPE.EDIT && <div>Edit</div>}
+            </Modal>
         </div>
     );
 };
